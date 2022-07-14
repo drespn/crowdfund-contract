bb09f139115c2118961963a3db2f703c6b4c2ce7
Diego Espinosa <diego.r.espinosa@duke.edu>
2022-07-13 23:23:51 -0500

    include all depencies, wrap up this project

---
+++ nodes/index.js
@@ -0,0 +1,102 @@
+const { Connection, Keypair, PublicKey, SystemProgram, Transaction,TransactionInstruction, sendAndConfirmTransaction, LAMPORTS_PER_SOL } = require("@solana/web3.js");
+const { fs } = require("mz");
+
+async function establishConnection(){
+    //default port when doing local work
+    const rpcUrl = 'http://localhost:8899';
+
+    const connection = new Connection(rpcUrl,'confirmed');
+
+    const version = await connection.getVersion();
+
+    console.log('Connection to cluster established',rpcUrl,version);
+}
+
+
+async function createKeyPairFromFile(){
+    //get secret key from computer in .json format into string
+    const secretKeyString = await fs.readFile("/Users/diegoespinosa/.config/solana/id.json",{encoding:'utf8'});
+
+    const secretKey = Uint8Array.from(JSON.parse(secretKeyString));
+
+    return Keypair.fromSecretKey(secretKey);
+}
+
+/*async function createAccount(){
+    const rpcUrl = 'http://localhost:8899';
+
+    const connection = new Connection(rpcUrl,'confirmed');
+
+    const signer = await createKeyPairFromFile();
+
+    //make the PDA from the original programID (we get from deployment)
+    const newAccountPubkey = await PublicKey.createWithSeed(
+        signer.publicKey,
+        "program1",
+        new PublicKey("EfyWUgrFnBHRcSZXo9v8UYEzVc26uHRFAbyB7A6pS1aE"),
+    );
+    //give enough to not disappear 
+    const lamports = await connection.getMinimumBalanceForRentExemption(2048);
+
+    //instruction for system program with seed
+    const newAccIx = SystemProgram.createAccountWithSeed(
+        {
+            fromPubkey: signer.publicKey,
+
+            basePubkey: signer.publicKey,
+
+            seed: "campaign1",
+
+            newAccountPubkey,
+
+            lamports, // todo
+
+            space: 2048,
+
+            programId : new PublicKey("EfyWUgrFnBHRcSZXo9v8UYEzVc26uHRFAbyB7A6pS1aE"),
+        }
+    )
+    
+    //make the new account finallly with a tx
+    const newAccTx = new Transaction().add(newAccIx);
+
+    console.log(`The address of campaign1 account is : ${newAccountPubkey.toBase58()}`);
+
+    await sendAndConfirmTransaction(connection,newAccTx, [signer]);
+
+
+}*/
+async function createAccount() {
+
+    const rpcUrl = 'http://localhost:8899';
+    connection = new Connection(rpcUrl, 'confirmed');
+    const signer = await createKeyPairFromFile();
+    const newAccountPubkey = await PublicKey.createWithSeed(
+      signer.publicKey,
+      "campaign1",
+      new PublicKey("EfyWUgrFnBHRcSZXo9v8UYEzVc26uHRFAbyB7A6pS1aE"),
+    );
+    const lamports = await connection.getMinimumBalanceForRentExemption(
+      1024,
+    );
+    const instruction = SystemProgram.createAccountWithSeed({
+      fromPubkey: signer.publicKey,
+      basePubkey: signer.publicKey,
+      seed: "campaign1",
+      newAccountPubkey,
+      lamports,
+      space: 1024,
+      programId : new PublicKey("EfyWUgrFnBHRcSZXo9v8UYEzVc26uHRFAbyB7A6pS1aE"),
+    });
+    const transaction = new Transaction().add(
+      instruction
+    );
+  
+    console.log(`The address of campaign1 account is : ${newAccountPubkey.toBase58()}`);
+  
+    await sendAndConfirmTransaction(connection, transaction, [signer]);
+  
+  }
+
+establishConnection();
+createAccount();
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 use solana_program::{
-    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, msg, pubkey::Pubkey,
+    account_info::{AccountInfo, next_account_info}, entrypoint, entrypoint::ProgramResult, msg, pubkey::Pubkey,
 };
+use borsh::{BorshSerialize, BorshDeserialize};
+
+pub mod error;
+use crate::error::CampaignError::InvalidInstruction;
 
 entrypoint!(process_instruction);
 fn process_instruction(
@@ -14,41 +18,74 @@ fn process_instruction(
         accounts.len(),
         instruction_data
     );
+
+    //process the passed in params
+
+    //Assuming first byte tells us what we want to do, lets match that
+    let(fbyte,rest) =  instruction_data.split_first().ok_or(InvalidInstruction)?;
+    //After error handling, need to list out the different scenarios
+    //If 0, start a campaign
+    match fbyte {
+        //start campiagn
+        0 => {
+            let iterable_accounts = &mut accounts.iter();
+
+        let initializer_acc = next_account_info(iterable_accounts)?;
+        //NOTE: First 4 bytes - either account to be created as campaign or funded
+        //NOTE: Next 4 bytes  - amount campaign wants
+        //NOTE: rest_of_data -  description string
+        let amount = rest
+            .get(..8)
+            .and_then(|slice| slice.try_into().ok())
+            .map(u64::from_le_bytes)
+            .ok_or(InvalidInstruction)?;
+        //getting the description
+        //make a utf8 from the rest of the slice to a vector and unwrap it
+        let description = String::from_utf8(rest[9..].to_vec()).unwrap();
+
+        //the data from the passed account info
+        let mut campaign_account_data = CampaignAccount::try_from_slice(&initializer_acc.data.borrow())?;
+
+            campaign_account_data.campaign_amounts = amount;
+            campaign_account_data.campaign_descriptions = description;
+            campaign_account_data.campaign_fulfilled = 0;
+
+            campaign_account_data.serialize(&mut &mut initializer_acc.data.borrow_mut()[..])?;
+
+                  
+         }
+        2 =>{
+            let iterable_accounts = &mut accounts.iter();
+
+            let initializer_acc = next_account_info(iterable_accounts)?;
+
+            //after getting the account, get the data
+            let campaign_account_data = CampaignAccount::try_from_slice(&initializer_acc.data.borrow())?;
+
+            msg!("{}",campaign_account_data.campaign_amounts-campaign_account_data.campaign_fulfilled);
+        }
+        _=>{}
+
+    }
+    
     Ok(())
 }
-
-#[cfg(test)]
-mod test {
-    use {
-        super::*,
-        assert_matches::*,
-        solana_program::instruction::{AccountMeta, Instruction},
-        solana_program_test::*,
-        solana_sdk::{signature::Signer, transaction::Transaction},
-    };
-
-    #[tokio::test]
-    async fn test_transaction() {
-        let program_id = Pubkey::new_unique();
-
-        let (mut banks_client, payer, recent_blockhash) = ProgramTest::new(
-            "bpf_program_template",
-            program_id,
-            processor!(process_instruction),
-        )
-        .start()
-        .await;
-
-        let mut transaction = Transaction::new_with_payer(
-            &[Instruction {
-                program_id,
-                accounts: vec![AccountMeta::new(payer.pubkey(), false)],
-                data: vec![1, 2, 3],
-            }],
-            Some(&payer.pubkey()),
-        );
-        transaction.sign(&[&payer], recent_blockhash);
-
-        assert_matches!(banks_client.process_transaction(transaction).await, Ok(()));
-    }
+//create campaign structure
+#[derive(BorshDeserialize,BorshSerialize,Debug)]
+pub struct CampaignAccount {
+    
+    pub campaign_owner: Pubkey,
+    pub campaign_amounts: u64,
+    pub campaign_descriptions: String,
+    pub campaign_fulfilled: u64
 }
+
+//defining the instructions based on what we want to do
+//1. Initialize a campaign
+//2. Fund a campaign
+//3. Get funds needed to reach goal
+//4. Widthdrawal funds and close campaign 
+
+
+
+
